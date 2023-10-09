e2d69ff00a95bdca111a5691858287f748c68ecd
mstafamajid <mstafa.00857387@gmail.com>
2023-10-10 01:49:56 +0300

    refactor the code

--- 2023-2024/week-2/Group A/app.py
+++ 2023-2024/week-2/Group A/app.py
@@ -1,17 +1,17 @@
 import random
-import time
 import tkinter
-from board import EightPuzzleGUI
+from board import EightPuzzleGUI  # Make sure you have the 'EightPuzzleGUI' class defined in 'board.py'
 
-Solution_path = []
+FILE_PATH="2023-2024\week-2\Group A\solution.txt"
 
 
 class PuzzleState:
+    GOAL_STATE = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
     def __init__(self, puzzle, parent=None, move=""):
         self.puzzle = puzzle
         self.parent = parent
         self.move = move
-        self.goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 
     def __eq__(self, other):
         return self.puzzle == other.puzzle
@@ -23,7 +23,7 @@ class PuzzleState:
         return "\n".join([" ".join(map(str, row)) for row in self.puzzle])
 
     def is_goal(self):
-        return self.puzzle == self.goal
+        return self.puzzle == self.GOAL_STATE
 
     def get_blank_position(self):
         for i in range(3):
@@ -31,7 +31,7 @@ class PuzzleState:
                 if self.puzzle[i][j] == 0:
                     return i, j
 
-    def get_legal_moves(self):
+    def get_moves(self):
         i, j = self.get_blank_position()
         moves = []
 
@@ -46,76 +46,55 @@ class PuzzleState:
 
         return moves
 
-    def generate_child(self, move):
+    def expand_state(self, move):
         i, j = self.get_blank_position()
         new_puzzle = [list(row) for row in self.puzzle]
 
         if move == "Up":
-            new_puzzle[i][j], new_puzzle[i - 1][j] = (
-                new_puzzle[i - 1][j],
-                new_puzzle[i][j],
-            )
+            new_puzzle[i][j], new_puzzle[i - 1][j] = new_puzzle[i - 1][j], new_puzzle[i][j]
         elif move == "Down":
-            new_puzzle[i][j], new_puzzle[i + 1][j] = (
-                new_puzzle[i + 1][j],
-                new_puzzle[i][j],
-            )
+            new_puzzle[i][j], new_puzzle[i + 1][j] = new_puzzle[i + 1][j], new_puzzle[i][j]
         elif move == "Left":
-            new_puzzle[i][j], new_puzzle[i][j - 1] = (
-                new_puzzle[i][j - 1],
-                new_puzzle[i][j],
-            )
+            new_puzzle[i][j], new_puzzle[i][j - 1] = new_puzzle[i][j - 1], new_puzzle[i][j]
         elif move == "Right":
-            new_puzzle[i][j], new_puzzle[i][j + 1] = (
-                new_puzzle[i][j + 1],
-                new_puzzle[i][j],
-            )
+            new_puzzle[i][j], new_puzzle[i][j + 1] = new_puzzle[i][j + 1], new_puzzle[i][j]
 
         return PuzzleState(new_puzzle, parent=self, move=move)
 
 
 def dfs_search(initial_state, initial_depth_limit=5):
     while initial_depth_limit <= 100:
-        stack = [(initial_state, 0)]  # Use a tuple to store both state and depth
+        stack = [(initial_state, 0)]  # Store state(node) and depth as a tuple
         visited = set()
-        depth_limit = initial_depth_limit  # Initialize depth limit for this iteration
+        depth_limit = initial_depth_limit
 
         while stack:
             current_state, depth = stack.pop()
             visited.add(current_state)
 
             if current_state.is_goal():
-                # Backtrack to reconstruct the correct path
-              
-                
-                    
-                    return (
-                        current_state  # Reverse the path to get it in the correct order
-                    )
+                return current_state
 
             if depth < depth_limit:  # Check if depth is within the limit
-                legal_moves = current_state.get_legal_moves()
-                for move in legal_moves:
-                    child_state = current_state.generate_child(move)
+                moves = current_state.get_moves()
+                for move in moves:
+                    child_state = current_state.expand_state(move)
                     if child_state not in visited:
                         stack.append((child_state, depth + 1))
 
-
-                        # Increment depth for child
-
         # If no solution is found within the current depth limit, increase it
-        print(initial_depth_limit)
+        print(f"Current depth limit {initial_depth_limit}")
         initial_depth_limit += 5
 
     # If no solution is found within the maximum depth limit, return None
     return None
 
 
-def save_solution_path_to_file(solution_path, filename):
-    solution_path = solution_path[::-1]
+def save_solution_path_to_file(solution_path, filename,directions):
+    solution_path = solution_path[::-1]  # Reverse the order of the path
     try:
         with open(filename, "a") as file:
-            step_number = 1 # Initialize step number
+            step_number = 1  # Initialize step number
             for i in range(len(solution_path)):
                 state = solution_path[i]
                 direction = directions[i]
@@ -127,73 +106,67 @@ def save_solution_path_to_file(solution_path, filename):
                     file.write(row_str + "\n")
                 file.write("\n")
                 step_number += 1
-                
-            file.write(f"cost: {len(solution_path)} steps to reach the goal")
+
+            file.write(f"Cost: {len(solution_path)} steps to reach the goal")
 
         print(f"Solution path saved to {filename}")
     except Exception as e:
         print(f"Error saving solution path to {filename}: {str(e)}")
-        
-        
+
+
 def check_solvable(puzzle):
-    count_inv=0
-    
-    for i in range(0,9):
-        for j in range(i+1,9):
-            if (puzzle[i]>puzzle[j]):
-                if(puzzle[j]!=0):
-                    count_inv+=1
+    count_inv = 0
 
-    if count_inv%2==0:
-        return True
-    return False
-            
+    for i in range(0, 9):
+        for j in range(i + 1, 9):
+            if puzzle[i] > puzzle[j]:
+                if puzzle[j] != 0:
+                    count_inv += 1
+
+    return count_inv % 2 == 0
 
 
-numbers = list(range(9))
+def main():
+    numbers = list(range(9))
+    random.shuffle(numbers)
+    print(numbers)
 
-# Shuffle the numbers randomly
-random.shuffle(numbers)
-print(numbers)
-# Reshape the shuffled list into a 3x3 2D array
-three_by_three_array = [numbers[i : i + 3] for i in range(0, len(numbers), 3)]
+    three_by_three_array = [numbers[i: i + 3] for i in range(0, len(numbers), 3)]
 
-initial_state = PuzzleState(three_by_three_array)
-with open("2023-2024\week-2\Group A\solution.txt", "w") as file:
-    # Write the initial board label
-    file.write("Initial board:\n")
+    initial_state = PuzzleState(three_by_three_array)
 
-    # Write the 3x3 array to the file
-    for row in three_by_three_array:
-        # Join the elements in each row as strings and separate them with spaces
-        row_str = "  ".join(map(str, row))
-        file.write(row_str + "\n")
+    with open(FILE_PATH, "w") as file:
+        file.write("Initial board:\n")
+        for row in three_by_three_array:
+            row_str = "  ".join(map(str, row))
+            file.write(row_str + "\n")
 
-if check_solvable(numbers):
-    goal_state = dfs_search(initial_state)
+    if check_solvable(numbers):
+        goal_state = dfs_search(initial_state)
 
-    if goal_state:
-        goals_path = []
-        directions = []
-        print("Solution found!")
-        while goal_state.parent:
-            goals_path.append(goal_state.puzzle)
-            directions.append(goal_state.move)
-            goal_state = goal_state.parent
+        if goal_state:
+            goals_path = []
+            directions = []
+            print("Solution found!")
+            while goal_state.parent:
+                goals_path.append(goal_state.puzzle)
+                directions.append(goal_state.move)
+                goal_state = goal_state.parent
 
-        if __name__ == "__main__":
-            root = tkinter.Tk()
-            app = EightPuzzleGUI(root, goals_path[::-1])
-            root.mainloop()
+            if __name__ == "__main__":
+                root = tkinter.Tk()
+                app = EightPuzzleGUI(root, goals_path[::-1])
+                root.mainloop()
 
-        directions = directions[::-1]
-        save_solution_path_to_file(goals_path, "2023-2024\week-2\Group A\solution.txt")
-
+            directions = directions[::-1]
+            save_solution_path_to_file(goals_path, FILE_PATH,directions)
+        else:
+            print("No solution within the maximum depth limit.")
     else:
-        print("No solution within the maximum depth limit.")
-else:
-    print("this puzzle isn't solvable")
-    with open("2023-2024\week-2\Group A\solution.txt", "w") as file:
-    # Write the initial board label
-        file.write("the board isn't solvable:\n")
-    # Extract the final state from the goal_states list
+        print("This puzzle isn't solvable")
+        with open(FILE_PATH, "w") as file:
+            file.write("The board isn't solvable:\n")
+
+
+if __name__ == "__main__":
+    main()
